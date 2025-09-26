a226245e1713f3d66bb31844fc8964e101fe25c5
Biosha <guegui@ymail.com>
2025-09-26 16:35:37 +0200

    vite build

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import DinozCalculator from "./DinozCalculator.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <DinozCalculator />
   </StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,8 @@ export default defineConfig({
   plugins: [
       tailwindcss(),
       react()],
+    build: {
+        outDir: 'docs',
+        emptyOutDir: true, // also necessary
+    }
 })
